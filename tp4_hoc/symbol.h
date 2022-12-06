6bd6ab652c81bafcb9bac77f54d72aedcf181386
Lafie-rage <corentin.destrez62@gmail.com>
2022-12-06 15:21:02 +0100

    Hoc 1.2 donné par le prof

---
+++ tp4_hoc/symbol.h
@@ -0,0 +1,61 @@
+/**
+ *	\file		symbol.h
+ *	\brief		Spécification de la gestion des symboles
+ *	\date		22 Janvier 2021
+ *	\author		Samir El Khattabi
+ */
+#ifndef _SYMBOL_H_
+#define _SYMBOL_H_
+/**
+ *	\typedef	generic
+ *	\brief		Définition du type de donnéés générique
+ */
+typedef void * generic;
+/**
+ *	\typedef	symbol_t
+ * 	\brief		Définition du type de données "symbole" de type struct
+ */
+typedef struct symbol {
+    char	*name;			// Nom du symbole : identifiant
+    short	clas;			// Classe du symbole : DAT, CST, VAR
+    short	type;			// Type du symbole : ENTIER, REEL, UNDEF
+    union {
+        generic pValue;     // Pointeur générique sur la valeur du symbole
+        double (*pFct)();	// Pointeur sur Fct
+    } U;
+    short	size;			// Taille mémoire occupée par la valeur
+    struct	symbol *next;	// Pointeur sur symbole suivant
+} symbol_t;
+/**
+ *	\typedef	pSymbol_t
+ *	\brief		Définition du type de données "pointeur sur symbole"
+ */
+typedef struct symbol* pSymbol_t;
+/**
+ *	\def		SYMBOL_NULL
+ *	\brief		Définition du symbole nul
+ */
+#define SYMBOL_NULL ((pSymbol_t) 0)
+/**
+ * 	\fn			symbol_t* installSymbol (char* tokenName, short tokenClas, short tokenType,
+                                            short tokenSize, generic pValue)
+ * 	\brief		Insérer un nouveau symbole en tête de la liste des symboles
+ *	\return		pointeur sur symbole inséré
+ */
+symbol_t* installSymbol (char* tokenName, short tokenClas, short tokenType,
+                        short tokenSize, generic tokenPtrValue);
+/**
+ *	\fn         symbol_t* lookUpSymbol (const char* tokenName)
+ *	\brief		Rechercher un symbole dans la liste des symboles
+ *	\return		pointeur sur symbole recherché ou NULL si non trouvé
+ */
+symbol_t* lookUpSymbol (const char* tokenName);
+/**
+ *	\fn         void installDefaultSymbols(void)
+ *	\brief		Installe les symboles par défaut dans la table des symboles : 
+ *				<UL><LI>Constantes : PI, E, ...</LI>
+ *					<LI>Fonctions mathématiques : sin(), cos(), ...
+ *				</UL>
+ */
+void installDefaultSymbols(void);
+#endif /* _SYMBOL_H_ */
---
+++ version_samir/hoc.h
@@ -0,0 +1,37 @@
+#ifndef _HOC_H_
+#define _HOC_H_
+#ifdef _HOC_L_			// inclusion par hoc.l
+#include "symbol.h"
+#include "io.h"
+#include "hoc.tab.h"
+#endif
+#ifdef _HOC_Y_			// inclusion par hoc.y
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "symbol.h"
+#include "io.h"
+#endif
+#ifdef _SYMBOL_C_		// inclusion par symbol.c
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "symbol.h"
+#include "hoc.tab.h"
+#include "io.h"
+#endif
+#ifdef _COLOR_C_		// inclusion par color.c
+#include <stdio.h>
+#include <libgen.h>
+#include <stdarg.h>
+#include "color.h"
+#endif
+#ifdef _IO_C_			// inclusion par io.c
+#include <stdarg.h>
+#include <stdio.h>
+#include "symbol.h"
+#include "color.h"
+#include "io.h"
+#endif
+
+#endif /* _HOC_H_ */
